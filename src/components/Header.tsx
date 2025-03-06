d10d3ce9abea21a2e3e74df9f3be2250e6fc9c93
OrwinZavaleta <orwinzavaleta@gmail.com>
2025-03-06 21:47:29 +0100

    i think the error is corrected, i have changed the <a> links with the <Link> in the React-router

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,38 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <>
       <nav className="bg-[#0f0d23] py-4 px-6 shadow-md">
         <ul className="flex items-center space-x-8 text-white">
           <li className="mr-auto">
-            <a href="/home">
+            <Link to="/home">
               <img src="/logo.png" alt="logo image" className="h-10 w-auto" />
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="/home"
+            <Link
+              to="/home"
               className="font-medium hover:text-light-100 transition-colors"
             >
               Home
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="/search"
+            <Link
+              to="/search"
               className="font-medium hover:text-light-100 transition-colors"
             >
               Search
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="/favorites"
+            <Link
+              to="/favorites"
               className="font-medium hover:text-light-100 transition-colors"
             >
               Favorites
-            </a>
+            </Link>
           </li>
         </ul>
       </nav>
